aee4f59682a4a1ed6f4e7b833d15df0345d9116e
Mr-p1gnum <andruyha5000@gmail.com>
2026-03-20 20:48:14 +0400

    Запустил

--- todo-app/app.py
+++ todo-app/app.py
@@ -43,4 +43,4 @@ def delete_task_by_id(task_id):
     return jsonify({'error': 'Task not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
